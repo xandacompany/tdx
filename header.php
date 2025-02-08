38dc88ee084a67521a316cfb5294841ee18f3962
XandA <102875868+xandacompany@users.noreply.github.com>
2025-02-07 21:02:18 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="date" content="2024-09-05">
+    <link rel="canonical" href="https://URL.com">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="alternate" hreflang="es" href="https://URL.com">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-Content-Type-Options" content="nosniff">
+    <meta http-equiv="Referrer-Policy" content="no-referrer-when-downgrade">
+    <meta name="google-adsense-account" content="ca-pub-2107247831846945">
+
+    <title>TDX Digital Acceleration</title>
+
+    <!-- SEO -->
+    <meta name="description" content="En TDX transformamos negocios con soluciones tecnológicas innovadoras. Ofrecemos desarrollo web y ecommerce, integración de chatbots y WhatsApp para optimizar operaciones y aumentar la eficiencia. Como expertos en servicios de TI en Colombia y Guatemala, ayudamos a empresas en América a acelerar su transformación digital con tecnología de vanguardia. ¡Descubre cómo TDX puede transformar tu negocio!">
+    <meta name="keywords" content="TDX">
+    <meta name="author" content="Asiel Jiménez, TDX">
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta name="subject" content="">
+    <meta name="rating" content="General">
+
+    <!-- Open Graph para Redes Sociales -->
+    <meta property="og:title" content="TDX">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://URL.com">
+    <meta property="og:description" content="">
+    <meta property="og:locale" content="es_MX">
+    <meta property="og:site_name" content="TDX">
+    <meta property="og:image" content="https://www.URL.com/assets/images/miniatura_redes_socilaes.png.png">
+
+    <!-- Twitter Cards -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="TDX">
+    <meta name="twitter:description" content="En TDX transformamos negocios con soluciones tecnológicas innovadoras. Ofrecemos desarrollo web y ecommerce, integración de chatbots y WhatsApp para optimizar operaciones y aumentar la eficiencia. Como expertos en servicios de TI en Colombia y Guatemala, ayudamos a empresas en América a acelerar su transformación digital con tecnología de vanguardia. ¡Descubre cómo TDX puede transformar tu negocio!">
+    <meta name="twitter:site" content="@TDX">
+    <meta name="twitter:image" content="https://www.URL.com/assets/images/miatura.png">
+    <meta property="og:image:alt" content="TDX">
+
+    <!-- Favicon -->
+    <link rel="icon" href="assets/images/iconos/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/images/iconos/favicon.ico" type="image/x-icon">
+
+    <!-- Apple Touch Icon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/iconos/apple-touch-icon-180x180.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/images/iconos/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/images/iconos/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/images/iconos/apple-touch-icon-76x76.png">
+
+    <!-- Iconos para Android -->
+    <link rel="icon" sizes="192x192" href="assets/images/iconos/android-chrome-192x192.png">
+    <link rel="icon" sizes="512x512" href="assets/images/iconos/android-chrome-512x512.png">
+
+    <!-- Otros formatos -->
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/iconos/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/iconos/favicon-16x16.png">
+
+    <!-- Manifest para PWA (Progressive Web App) -->
+    <link rel="manifest" href="assets/scripts/tdx.webmanifest">
+
+    <!-- Meta etiquetas para mobile -->
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+
+    <!-- Datos Estructurados JSON-LD -->
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "NewsMediaOrganization",
+            "name": "TDX Digital Acceleration",
+            "url": "https://www.URL.com",
+            "logo": "https://www.tdx.com/assets/images/iconos/android-chrome-512x512.png",
+            "sameAs": [
+            "https://www.facebook.com/tdx.oficial/",
+            "https://www.instagram.com/tdx_oficial/"
+            ],
+            "description": "En TDX transformamos negocios con soluciones tecnológicas innovadoras. Ofrecemos desarrollo web y ecommerce, integración de chatbots y WhatsApp para optimizar operaciones y aumentar la eficiencia. Como expertos en servicios de TI en Colombia y Guatemala, ayudamos a empresas en América a acelerar su transformación digital con tecnología de vanguardia.  ¡Descubre cómo TDX puede transformar tu negocio!",
+            "founder": "",
+            "foundingDate": "",
+            "contactPoint": {
+            "@type": "",
+            "contactType": "",
+            "email": "",
+            "url": ""
+            }
+        }
+    </script>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="assets/styles/main.css">
+
+    <!-- Carrusel -->
+    <link rel="stylesheet" href="assets/carrusel/demo/demo.css">
+    <link rel="stylesheet" href="assets/carrusel/dist/jquery.flipster.min.css">
+    <script src="assets/carrusel/demo/jquery.min.js"></script>
+    <script src="assets/carrusel/dist/jquery.flipster.min.js"></script>
+
+    <!-- Aos -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    
+<?php wp_head(); ?>
+</head>
+<body class="body">
